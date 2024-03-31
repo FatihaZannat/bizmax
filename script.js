0f52103c1180415743f4684257381da1cc79b647
Fatiha Zannat <fatihazannat.n@gmail.com>
2024-03-31 22:04:30 +0600

    background image  change

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(()=>{
     
-$('.slider-btn-1').click(()=>{
-   $('.bg-img').css('background-image','url("./image/Slider--2.png")')
+$('.slider-btn-1').click(function(){
+   $('.bg-img').css('background-image','url("./image/banner-8 1.png")')
+   console.log('object');
+})
+$('.slider-btn-2').click(function(){
+   $('.bg-img').css('background-image','url("./image/Slider--1.png")')
    console.log('object');
 })
 
 
+// $('.your-class').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
 })
+
