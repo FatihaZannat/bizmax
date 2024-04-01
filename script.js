2213365eae595b92f44a298290e9de1b4f6b6b4a
Fatiha Zannat <fatihazannat.n@gmail.com>
2024-04-01 11:05:32 +0600

    social

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-$(document).ready(()=>{
-    
-$('.slider-btn-1').click(function(){
-   $('.bg-img').css('background-image','url("./image/banner-8 1.png")')
-   console.log('object');
-})
-$('.slider-btn-2').click(function(){
-   $('.bg-img').css('background-image','url("./image/Slider--1.png")')
-   console.log('object');
-})
+$(document).ready(() => {
 
-// search button
-$('.search').click(function(){
-   $('.search-details').toggle()
-})
+   $('.slider-btn-1').click(function () {
+      $('.bg-img').css('background-image', 'url("./image/banner-8 1.png")')
+      console.log('object');
+   })
+   $('.slider-btn-2').click(function () {
+      $('.bg-img').css('background-image', 'url("./image/Slider--1.png")')
+      console.log('object');
+   })
+
+   // search button
+   // $('.search').click(function (e) {
+      
+   //       $('.search-details').toggle()
+      
+
+   // })
+
+   $('.search').click(function (e) {
+      if ($('.search-details').css('display') === ('none')) {
+         $('.search-details').css('display', 'block')
+      }else{
+         $('.search-details').css('display', 'none')
+      }
+
+   })
+
 })
 
